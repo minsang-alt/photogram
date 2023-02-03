ad820e63e9c74aa31b81c33111e240f9ab0d4ad9
minsang-alt <mazinger97@naver.com>
2023-02-03 23:40:27 +0900

    회원수정 유효성검사

--- src/main/java/com/cos/photogramstart/handler/ControllerExceptionhandler.java
+++ src/main/java/com/cos/photogramstart/handler/ControllerExceptionhandler.java
@@ -1,11 +1,12 @@
 package com.cos.photogramstart.handler;
 
-import java.util.Map;
-
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cos.photogramstart.handler.ex.CustomValidationApiException;
 import com.cos.photogramstart.handler.ex.CustomValidationException;
 import com.cos.photogramstart.util.Script;
 import com.cos.photogramstart.web.dto.auth.CMRespDto;
@@ -22,6 +23,14 @@ public class ControllerExceptionhandler {
 		//3.Android 통신 -CMRespDto
 		return Script.back(e.getErrorMap().toString());
 		//return new CMRespDto<Map<String,String>>(-1,e.getMessage(),e.getErrorMap());
+	
+	
+	}
+	
+	@ExceptionHandler(CustomValidationApiException.class)//이 예외가 발생하는 모든 exception을 이 함수가 가져간다.
+	public ResponseEntity<?> validationApiException(CustomValidationException e) {
+	
+		return new ResponseEntity<>(new CMRespDto<>(-1,e.getMessage(),e.getErrorMap()),HttpStatus.BAD_REQUEST);		
 	
 	
 	}
--- src/main/resources/static/js/update.js
+++ src/main/resources/static/js/update.js
@@ -1,6 +1,6 @@
 // (1) 회원정보 수정
 function update(userId,event) {
-
+event.preventDefault();//폼 태그 액션을 막기
 let data = $("#profileUpdate").serialize();
 console.log(data);
 $.ajax({
@@ -9,11 +9,12 @@ $.ajax({
 	data:data,
 	contentType:"application/x-www-form-urlencoded; charset=utf-8",
 	dataType:"json"
-}).done(res=>{
-	console.log("update 성공");
+}).done(res=>{//HttpStatus 상태코드 200번대
+	console.log("성공",res);
 	location.href = `/user/${userId}`;
-}).fail(error=>{
-	console.log("update 실패");
+}).fail(error=>{//HttpStatus 상태코드 200번대가 아닐때	
+	
+	alert(JSON.stringify(error.responseJSON.data));
 });
 
 
